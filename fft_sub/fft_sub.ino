24e0065525143e32c4c6219a9f3dd0ad1268b890
takahashi <ryo971211@gmail.com>
2019-12-21 13:28:12 +0900

    ハニング窓を作成

--- fft_sub/fft_sub.ino
+++ fft_sub/fft_sub.ino
@@ -20,6 +20,8 @@ const int BUF_NUM = 2;                              //バッファの数
 
 /* グローバル変数 */
 static float32_t buf[BUF_NUM][DATA_NUM];            //受信したデータを保持するバッファ
+static float32_t time_sig[2*FFT_POINT];             //時間領域波形、複素数
+static float32_t window[2*FFT_POINT];               //窓関数
 
 void setup() {
   //サブコア起動
@@ -31,7 +33,13 @@ void setup() {
       buf[i][j] = 0.0;
     }
   }
-  
+
+  //窓関数を作成
+  float32_t coe;
+  for(int i = 0;i < 2*FFT_POINT;i+=2){
+    coe = ((float32_t)i) / ((float32_t)(2*FFT_POINT));
+    window[i] = 0.5 - 0.5*arm_cos_f32(2.0*PI*coe);
+  }
 }
 
 void loop() {
@@ -44,7 +52,7 @@ void loop() {
 
   //受け取ったデータを移し替える
   for(int i = 0;i < DATA_NUM;++i){
-    buf[buf_pos][i] = r_buf[i];
+    buf[buf_pos][i] = (float32_t)r_buf[i];
   }
   buf_pos = (buf_pos + 1) % BUF_NUM;
   
